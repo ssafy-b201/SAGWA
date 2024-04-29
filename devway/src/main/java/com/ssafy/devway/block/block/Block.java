cdcbeee56d1021559c0bfbe638824044fd022b07
김준섭 <wns1915@naver.com>
2024-04-29 15:39:37 +0900

    feat: add structure
    Former-commit-id: abc058f78d65c45f21d5ed5b899c8b1beed58ee5

---
+++ devway/src/main/java/com/ssafy/devway/block/block/Block.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:26e9f83869dc86591a758ec90f93b008621675a2e63dc681d54ec6f27ce4f404
+size 525
--- devway/src/main/java/com/ssafy/devway/domain/block/SimpleBlock.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a3d3fc236650108b6bb38ccb628d4d0413230b2b4c50c08e8eacbfdd99f9e2fa
-size 700
