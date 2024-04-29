3f5ce8d1c907b0784f1c65bf1dd7621a809fc1ac
김동학 <kdh19583@gmail.com>
2024-04-29 14:21:27 +0900

    chore: fix code convention
    Former-commit-id: 4b32a7b334efffecc580f8f1a81e6970aad4afc3

--- devway/src/main/java/com/ssafy/devway/domain/block/SimpleBlock.java
+++ devway/src/main/java/com/ssafy/devway/domain/block/SimpleBlock.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c37498de743c3313f0aecba786989af8a8231cf614dcb569ccc98123a82b3be3
-size 699
+oid sha256:a3d3fc236650108b6bb38ccb628d4d0413230b2b4c50c08e8eacbfdd99f9e2fa
+size 700
