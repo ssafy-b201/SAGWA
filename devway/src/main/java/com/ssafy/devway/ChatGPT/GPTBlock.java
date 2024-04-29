175ae9b77f97513a677aa0cf8332abf0e36b587d
김준섭 <wns1915@naver.com>
2024-04-29 22:06:20 +0900

    feat: add gpt modes and fix the connection error
    Former-commit-id: 1f0825930d0280f8a6e9c168bd772c58dd9ee4f5

--- devway/src/main/java/com/ssafy/devway/ChatGPT/GPTBlock.java
+++ devway/src/main/java/com/ssafy/devway/ChatGPT/GPTBlock.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9a6efd725a2c0dcd1ba548f964d8037a55598ff790e1b18fb0150c0d01f71eb
-size 1880
+oid sha256:bc39a207dd026f870c15dc23e731a8cada4cfebc6e5dfb929c20498cff666f47
+size 2969
---
+++ devway/src/main/java/com/ssafy/devway/ChatGPT/GPTMode.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6fc2b3c86b748c8fd976da9b810fcc46ea6cce4edd4bd51518b5a803bed8925d
+size 1513
