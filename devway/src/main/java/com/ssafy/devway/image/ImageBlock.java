91aece6a95a783063298e395ab7169fc7c4ecdac
donghakk <kdh19583@gmail.com>
2024-05-02 07:15:41 +0900

    feat: add method on ImageBlock
    Former-commit-id: 503c618a6593d366901ab961f878884f4cecaec9

--- devway/src/main/java/com/ssafy/devway/image/ImageBlock.java
+++ devway/src/main/java/com/ssafy/devway/image/ImageBlock.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58becce9d3fb1d4609839adafe97292071e283cd28ed6cbec9a57dc8c43805d5
-size 2122
+oid sha256:2289a9c98917aec24902ef47cec8a9406a8041df078967c603d005179660771d
+size 2624
