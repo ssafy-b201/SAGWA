f9e23edef50526a62f78735a2dd7c13c2f396880
김준섭 <wns1915@naver.com>
2024-05-01 16:40:54 +0900

    feat: add STT
    Former-commit-id: 642102da93e7eb5151bc64b35de51b97213166c8

---
+++ devway/src/main/java/com/ssafy/devway/GPT/GPTMode.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ae038f0773535d5180045c94bb4ab777f1164a9dd65b655791a92ad3c4db15b3
+size 1509
---
+++ devway/src/main/java/com/ssafy/devway/STT/STTBlock.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:68605d3bc89d8190c168dc824f5302beee906d0bb24678b693d3c2e2b4c3793f
+size 2493
