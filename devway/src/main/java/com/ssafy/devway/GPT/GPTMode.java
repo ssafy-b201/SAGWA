c6a3179fd90ce63712609980c77b4af88311c26d
김준섭 <wns1915@naver.com>
2024-05-01 17:51:20 +0900

    chore: fix typo
    Former-commit-id: 7638b7966dfbec61b9e4f57879c9b6546cc634af

--- devway/src/main/java/com/ssafy/devway/GPT/GPTMode.java
+++ devway/src/main/java/com/ssafy/devway/GPT/GPTMode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae038f0773535d5180045c94bb4ab777f1164a9dd65b655791a92ad3c4db15b3
+oid sha256:3603512ff95c48932873ffc70f8c2f0f7487afb2605377f63d8c3781cc7ebc27
 size 1509
