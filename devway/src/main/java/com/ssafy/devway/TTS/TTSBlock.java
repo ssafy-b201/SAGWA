d805bf9fdac3d30be6b0414ccba5adba21876eb9
김준섭 <wns1915@naver.com>
2024-05-01 17:42:49 +0900

    feat: add TTS
    Former-commit-id: 841ba480752fb34883c0392600ef71d40945ad23

---
+++ devway/src/main/java/com/ssafy/devway/TTS/TTSBlock.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f7a191474d0ee4743f9f1fec3f6bf771a99c7afb9ebf6c8e83a33fa111341ee
+size 1872
---
+++ devway/src/main/java/com/ssafy/devway/TTS/TTSCountry.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:990c0ca54758ff3c82eeb2cf21fd90985199862cf2cfd804aea25577e86227b1
+size 890
