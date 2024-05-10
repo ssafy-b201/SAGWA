a44323b2b7867d0e494550dcd55e2096a365ec61
양성주 <ztjdwnz@naver.com>
2024-05-10 17:56:01 +0900

    [feat] splash, signin
    Former-commit-id: ea6c8e14c4d95a53512da80ad74a73176bccf92c

--- front/app/build.gradle.kts
+++ front/app/build.gradle.kts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ade86402d97f0f76532ccba459e24ccc20fb234b3b02e7bed32d44eeeceb4053
-size 1033
+oid sha256:a750f6209f26a25d662d904bd321c00470f946323005db80055819e48b72f159
+size 2133
