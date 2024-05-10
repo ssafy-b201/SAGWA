812609f76ac8388f40bee29d12948182dd0de51f
양성주 <ztjdwnz@naver.com>
2024-05-10 14:27:13 +0900

    [feat] member
    Former-commit-id: ffb0ca3166d72501fa44f57486e035033975b89e

--- devway/src/main/java/com/ssafy/devway/domain/member/entity/Member.java
+++ devway/src/main/java/com/ssafy/devway/domain/member/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb71107e62449751cafdf94bf785fd2f84787c1b74b54e09a9d4a6597bec1bae
-size 656
+oid sha256:1b931491b0d4498c93af805359e297f5e29fa4950bc854afb22c07f2df46d7f0
+size 716
