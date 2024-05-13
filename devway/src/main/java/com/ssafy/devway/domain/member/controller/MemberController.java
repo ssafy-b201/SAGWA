e7fcca189477910c7d150b5a5fd2f6d4d9c21f6c
양성주 <ztjdwnz@naver.com>
2024-05-13 13:04:37 +0900

    [fix] fix annotation
    Former-commit-id: 119864f6287e6305294b17b08378043a39a5d150

--- devway/src/main/java/com/ssafy/devway/domain/member/controller/MemberController.java
+++ devway/src/main/java/com/ssafy/devway/domain/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9c1ab26bf31019df2ba1ec008632aee1a60508026d677021770742f1690cd23
-size 1365
+oid sha256:e74125229bd7e33e87e2f50785567d71ccd98a6440551fca22f6fabf4259aea7
+size 1396
