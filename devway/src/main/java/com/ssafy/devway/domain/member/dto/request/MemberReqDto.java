4b10dba507e513dce76928459e9f4782db7e8c25
양성주 <ztjdwnz@naver.com>
2024-05-13 16:10:45 +0900

    [fix] fix MemberReqDto
    Former-commit-id: 7f2a4457337bbb9a40a0893a051da08ca90a3650

--- devway/src/main/java/com/ssafy/devway/domain/member/dto/request/MemberReqDto.java
+++ devway/src/main/java/com/ssafy/devway/domain/member/dto/request/MemberReqDto.java
@@ -1,12 +1,16 @@
 package com.ssafy.devway.domain.member.dto.request;
 
+import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Getter
 public class MemberReqDto {
-    String email;
+    @JsonProperty("email")
+    private String email;
 }
