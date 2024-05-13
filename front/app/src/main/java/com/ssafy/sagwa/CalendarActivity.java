f7dc0f30249f4167c6478abd55e58565432f45eb
박소현 <s_971001@naver.com>
2024-05-13 17:36:58 +0900

    [chore] fix manifest
    Former-commit-id: bc0aa611a9a535c321c328e6af83d89bd8c3d050

--- front/app/src/main/java/com/ssafy/sagwa/CalendarActivity.java
+++ front/app/src/main/java/com/ssafy/sagwa/CalendarActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d447ed995150e3bc4af74772599ee1019932b75dc2fd82d1f5d92adee0809562
-size 843
+oid sha256:2ff4cea8ddda6979d99cef0cfe085f363dcd7f9ed3db34e140e5c256c852eca3
+size 855
