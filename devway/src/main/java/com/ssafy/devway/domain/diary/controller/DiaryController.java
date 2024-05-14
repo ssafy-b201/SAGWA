1453cc3963eecc0eb015b0275e14a4b5d1206551
김준섭 <wns1915@naver.com>
2024-05-14 15:52:20 +0900

    [fix] fix for deploy
    Former-commit-id: b5974c1476bf6cc9eae4b7e80a7de3e094b47742

--- devway/src/main/java/com/ssafy/devway/domain/diary/controller/DiaryController.java
+++ devway/src/main/java/com/ssafy/devway/domain/diary/controller/DiaryController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84ae837bf9662853fb3cbfdef8037ec037dce079824ad67ed4afa147af291607
-size 2836
+oid sha256:d6992bee06b4ccb4ece60910cbe08a79e6a2473bae674cd8136e223aeec2bba8
+size 2484
--- devway/src/main/java/com/ssafy/devway/domain/diary/service/DiaryService.java
+++ devway/src/main/java/com/ssafy/devway/domain/diary/service/DiaryService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8de6331658dfae8f5342ac91e54452edb2a424f339e66d238fcd1b30d232e16b
-size 8568
+oid sha256:9b442e8dfe66884636acec1ed16451d80ebbfc5f83fca8e9e7b5c2c56f36bab4
+size 7030
