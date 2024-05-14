9e6490c80f05f64be2f1b43bc61e843fb6204217
김준섭 <wns1915@naver.com>
2024-05-14 14:02:35 +0900

    [fix] fix for deploy
    Former-commit-id: 94ce4473b345c8a9c15177bf4d9e916bc79e81ef

--- devway/src/main/java/com/ssafy/devway/domain/diary/service/DiaryService.java
+++ devway/src/main/java/com/ssafy/devway/domain/diary/service/DiaryService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d66819a836ad51ce98cf3d30957b54efd788c14ce99f277c01625d89af6c6288
-size 8484
+oid sha256:8de6331658dfae8f5342ac91e54452edb2a424f339e66d238fcd1b30d232e16b
+size 8568
