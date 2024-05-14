3e8599629283d089a1c202c727c8e08ce920ffdb
양성주 <ztjdwnz@naver.com>
2024-05-14 17:52:32 +0900

    [feat] call
    Former-commit-id: 6a53b5ba21a7e792444d06b0dd3f0a6fabb56621

--- front/app/build.gradle.kts
+++ front/app/build.gradle.kts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a750f6209f26a25d662d904bd321c00470f946323005db80055819e48b72f159
-size 2133
+oid sha256:da6ffc94702c384fec103b6b33a8d8aa5fa76f41f38c6e4a1beb47d002135eb7
+size 2134
