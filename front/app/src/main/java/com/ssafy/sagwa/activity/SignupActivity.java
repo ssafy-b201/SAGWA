57dd05f5adfe8e16dd590b2f4d3057f3a4aa034b
양성주 <ztjdwnz@naver.com>
2024-05-16 11:07:01 +0900

    [feat] add footer menu
    Former-commit-id: 4c9b8668c8f8a53885e81bb204e928182ae43e5f

--- front/app/src/main/java/com/ssafy/sagwa/activity/SignupActivity.java
+++ front/app/src/main/java/com/ssafy/sagwa/activity/SignupActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66583a0d99e40dbf8d8b411b957da7e4d4d8ecbe26a90be8e6c70ea0637251b8
-size 6099
+oid sha256:280a1cc79e0d6a8447eb77af7198cc949082b7e8f17d2b68a1d0f862f5269ac4
+size 6257
