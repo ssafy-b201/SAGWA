33109cc67fc1314a50ec15f47cff3875d3fa7e97
박소현 <s_971001@naver.com>
2024-05-17 17:58:46 +0900

    [chore] fix debug
    Former-commit-id: 3458f960fd543bea4b048aaa5231f95e8e7ef460

--- front/app/src/main/java/com/ssafy/sagwa/activity/CalendarActivity.java
+++ front/app/src/main/java/com/ssafy/sagwa/activity/CalendarActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd7b28147a9106b6dccab23fd4411e92db1513d0f2253d18f04a1c57cc5d6d01
-size 15369
+oid sha256:8198319e056dffdc533084feab593ae06704770e6056cb2d902cd4bed3735b17
+size 15575
