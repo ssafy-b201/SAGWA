ecfb186bb245a0d5e806145ffda998aa2067a44a
박소현 <s_971001@naver.com>
2024-05-23 23:22:40 +0900

    [design] writeDiary
    Former-commit-id: 36dd0e1f12b5e75b87431e09a312d3f25dfcd9ba

--- front/app/src/main/java/com/ssafy/sagwa/activity/WriteActivity.java
+++ front/app/src/main/java/com/ssafy/sagwa/activity/WriteActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0233a6055141687a0cc05ce92d40eee4583ef280e8a61d7fce303ac4c09dbbf4
-size 13392
+oid sha256:03065dfbee16a2a5893a06ca45f33addda8976a1de7d68871b4ef3e62b0276c7
+size 14364
