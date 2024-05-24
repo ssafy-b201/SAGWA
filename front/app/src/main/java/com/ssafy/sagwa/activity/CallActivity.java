88014f0b9243a138e74cbc25937bf96d55f17c21
양성주 <ztjdwnz@naver.com>
2024-05-24 13:57:36 +0900

    [feat] call 수정
    Former-commit-id: 8c52dfd2b315524b4397943284eb413f8a818737

--- front/app/src/main/java/com/ssafy/sagwa/activity/CallActivity.java
+++ front/app/src/main/java/com/ssafy/sagwa/activity/CallActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96cd15ebb4ff5f110b51445c16e0e1e65c00d2390754acf4d17f1b054cc8f779
-size 25205
+oid sha256:c66854f8efbc01325e4f7877a231c99e0791ceb1662f98adcda6adcb0b2a75ee
+size 22266
