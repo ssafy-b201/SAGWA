2e1920128671d0221560e9421d395dfc36ced8f2
박소현 <s_971001@naver.com>
2024-05-24 09:06:51 +0900

    [design] diary
    Former-commit-id: 898fcc89f9c43a9174b656ff190e5ff78267bb12

--- front/app/src/main/java/com/ssafy/sagwa/activity/CalendarActivity.java
+++ front/app/src/main/java/com/ssafy/sagwa/activity/CalendarActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ada5423d13d927867d30a039486575f87b841caf52a815c7b6af5e6e108a5356
-size 15644
+oid sha256:e914b4c0321797863b1b86da59463c32e831cc7133cc082ea1bdd4f6a2c2f52b
+size 18496
--- front/app/src/main/java/com/ssafy/sagwa/activity/WriteActivity.java
+++ front/app/src/main/java/com/ssafy/sagwa/activity/WriteActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03065dfbee16a2a5893a06ca45f33addda8976a1de7d68871b4ef3e62b0276c7
-size 14364
+oid sha256:8d21e4a1a8f7835d438b82bcfcf885b8355d7097d41f85ee229a9576f2c6fdc8
+size 14628
