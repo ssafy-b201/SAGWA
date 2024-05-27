bbc3353a5a5f509e6b01cf3ce4f56d6c3585017c
박소현 <s_971001@naver.com>
2024-05-27 18:35:32 +0900

    [chore] delete dialog
    Former-commit-id: db6d62398c59f4dbb825fec3ade4bc6afdc4d210

--- front/app/src/main/java/com/ssafy/sagwa/activity/DiaryDetailActivity.java
+++ front/app/src/main/java/com/ssafy/sagwa/activity/DiaryDetailActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27997220c4e790f8126618b0f31fd0824c971bc4cf20fe4becec6a63516ed4d7
-size 14292
+oid sha256:858fb692e4f900874e53c2059d516ac248f0642ac9dd4b7d1037499340effa3f
+size 13846
--- front/app/src/main/java/com/ssafy/sagwa/activity/WriteActivity.java
+++ front/app/src/main/java/com/ssafy/sagwa/activity/WriteActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:937cdd00d9ca49dbe19c8f8a7f393d6ecc7806fb2bcda07c35acc58ad44d2e97
-size 16658
+oid sha256:bac8753a8cde2715dee56618513fd809712aea9ee6d6914fc801d7e00c7c192e
+size 16614
