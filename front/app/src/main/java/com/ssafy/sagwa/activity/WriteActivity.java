52aaea239e0997045e85140d2903b5d14e679e0d
박소현 <s_971001@naver.com>
2024-05-27 14:08:43 +0900

    [design] add loading
    Former-commit-id: 765cc56a7abb160067817e5ca5118f82bbafe99c

--- front/app/src/main/java/com/ssafy/sagwa/activity/WriteActivity.java
+++ front/app/src/main/java/com/ssafy/sagwa/activity/WriteActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4269414d0e20efbdb148b3217ecee7d2069afbbe03409234bb030c72225c36aa
-size 16256
+oid sha256:937cdd00d9ca49dbe19c8f8a7f393d6ecc7806fb2bcda07c35acc58ad44d2e97
+size 16658
